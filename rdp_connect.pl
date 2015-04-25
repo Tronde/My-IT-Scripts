c446bad0b14a1f7e035864eb32cdfc9060bf7966
Tronde <tronde@my-it-brain.de>
2015-04-25 18:23:45 +0200

    'given/when'-Struktur auf if-elsif-else umgestellt.

--- rdp_connect.pl
+++ rdp_connect.pl
@@ -21,22 +21,24 @@ for my $nr (0 .. $#sorted_notizen)
 	{ print "[$nr] ", $sorted_notizen[$nr] }
 print "Was moechtest du tun? ($kommando{'eintragen'} Neuer Eintrag; $kommando{'verbinden'} Verbindung zu Host; $kommando{'loesche'} loescht Eintrag; $kommando{'bewege'} verschiebt Eintrag); $kommando{'beenden'} Exit: ";
 my $notiz = <STDIN>;
-given ( lc substr($notiz, 0, 1) ) {
-	when ($kommando{'verbinden'}) {}
-	when ($kommando{'beenden'}) { }
-	when ($kommando{'eintragen'}) {print "Gib den FQDN ein: ";
-			$notiz = <STDIN>;
-			append_file($datei, $notiz);
+if ( lc substr($notiz, 0, 1) eq $kommando{'verbinden'}) { }
+elsif (lc substr($notiz, 0, 1) eq $kommando{'beenden'}) { }
+elsif (lc substr($notiz, 0, 1) eq $kommando{'eintragen'}) {
+	print "Gib den FQDN ein: ";
+	$notiz = <STDIN>;
+	append_file($datei, $notiz);
 	} 
-	when ($kommando{'loesche'}) {
+elsif (lc substr($notiz, 0, 1) eq $kommando{'loesche'}) {
 		continue if length($notiz) == 2;
 		my $nr = int substr($notiz, 1);
 		splice(@notizen, $nr, 1) if $nr >= 0 and $nr <= $#notizen;
 		write_file($datei, @notizen);
 	}
-	when ($kommando{'bewege'}) {
+elsif ($lc substr($notiz, 0, 1) eq kommando{'bewege'}) {
 		continue if length($notiz) == length($kommando{'bewege'})+1;
 		my($von, $zu) = split ':',
 			substr($notiz, length($kommando{'bewege'}));
 	}
+else {
+	say "Hier wird mal ein Hilfe-Text stehen.";
 }
