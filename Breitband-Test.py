2a6b23f43b8a0219737c02477d275e630e0801ae
Tronde <tronde@my-it-brain.de>
2015-06-07 18:05:16 +0200

    Obsolete Datei gelöscht. ICMP-Funktionalität fertiggestellt.

--- Breitband-Test.py
+++ Breitband-Test.py
@@ -9,30 +9,33 @@
  Copyright 2015 by Jörg Kastning <joerg.kastning@my-it-brain.de>
 """
 
-import argparse, subprocess, csv
+import sys, argparse, subprocess, csv
 
 """
  Funktionen
 """
 
-def icmp(host):
+def icmp(host, count):
     """ Diese Prozedur sendet einige ICMP-Pakete an einen Host. """
-    ping_result = subprocess.check_output(["ping", "-c 4", host])
+    ping_result = subprocess.check_output(["ping", "-c", count, host])
     return ping_result
 
 def clientmodus():
     print "Das Pogramm befindet sich im Clientmodus."
+    print "Ausgabe von icmp_count:"
+    print(icmp_count)
     print "Ausgabe der ICMP-Requests:"
-    print(icmp(host))
+    print(icmp(host, icmp_count))
     
     f = open('workfile', 'a')
-    f.write(icmp(host))
+    f.write(icmp(host, icmp_count))
     f.close()
     f = open('workfile', 'r')
     #output = f.readlines()
     print "Ausgabe der Datei:"
     for line in f.readlines():
-        print(line)
+        sys.stdout.write(line)
+    #sys.stdout.flush()
     f.close()
 
 def servermodus():
@@ -45,7 +48,7 @@ def servermodus():
 parser = argparse.ArgumentParser(description=" Breitband-Test.py")
 parser.add_argument("-S", "--Server", dest="progmodus", action='store_true', default='False', help="Startet das Programm im Servermodus.")
 parser.add_argument("-P", "--Port", dest="port", default=500001, help="Port auf dem der Server hört bzw. zu dem sich der Client verbindet.")
-parser.add_argument("-C", dest="icmp_count", default=10, help="Anzahl ICMP-Pakete, welche an den Server gesendet werden.")
+parser.add_argument("-C", dest="icmp_count", default='10', help="Anzahl ICMP-Pakete, welche an den Server gesendet werden.")
 parser.add_argument("-H", "--Host", dest="Host", required=False, help="IP-Adresse an welche der Server-Socket gebunden wird bzw. zu dem sich der Client verbindet.")
 
 args = parser.parse_args()
