15418ce53fdc0635207700acafeb59dacc98f809
Tronde <tronde@my-it-brain.de>
2015-08-18 09:46:24 +0200

    Script to rsync remote directories to local machine.

---
+++ bash/rsync-doc.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# Name: rsync-doc.sh
+# Author: Joerg Kastning
+# License: GPLv3
+# URL: 
+#
+# This script synchronize remote directorys with a directory on the local host.
+# It was build to enable almost everyone to add new directories to the script,
+# which should be synced.
+
+# Variables ###################################################################
+
+SMB_SOURCE="//<FQDN>/<SHARE_NAME>"
+MOUNTPOINT="/mnt/<NAME>"
+CREDENTIALS="/<PATH>/<TO>/.smbcredentials"
+SOURCES=(/DIR1 /DIR2 )
+TARGET="/<TARGEDTDIR/"
+RSYNCCONF=(-az --delete-delay)
+
+# Functions ###################################################################
+
+do_mount()
+	{
+		mount -o credentials=$CREDENTIALS,ro,uid=$UID,gid=$GID -t cifs $SMB_SOURCE $MOUNTPOINT
+	}
+
+do_rsync()
+	{
+		for SOURCE in $SOURCES; do
+			rsync $RSYNCCONF $MOUNTPOINT$SOURCE $TARGET
+		done
+	}
+
+do_unmount()
+	{
+		umount $MOUNTPOINT
+	}
+
+# Program #####################################################################
+
+do_mount
+do_rsync
+do_unmount
+
+exit 0
