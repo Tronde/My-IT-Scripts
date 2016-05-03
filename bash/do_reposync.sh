e4b3c7fe191986e9cf143ff8a59c52eba722dd61
Tronde <tronde@my-it-brain.de>
2016-05-03 15:34:14 +0200

    Schleife zum Sync mehrerer Repos integriert

--- bash/do_reposync.sh
+++ bash/do_reposync.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 #
-# Beschreibung: Skript zur Anlage des RHEL-Repositories
+# Beschreibung: Skript zur Synchronisierung des RHEL-Repositories
 #               auf dem Spiegelserver
-# Autor: Joerg Kastning <joerg.kastning@uni-bielefel.de>
+# Autor: Joerg Kastning <joerg(Punkt)kastning(aet)uni-bielefeld.de>
 
 LOG="/var/log/do_reposync.log"
+REPOID=" "
+DOWNLOADPATH=" "
 
 echo \# `date +%Y-%m-%d` - START REPOSYNC \# > $LOG
 
-reposync --gpgcheck -l --repoid=rhel-7-server-rpms --download_path=/var/www/html/local-rhel-7-repo --downloadcomps --download-metadata -n >> $LOG
+for REPO in "${REPOID[@]}"
+  do
+    reposync --gpgcheck -l --repoid=$REPO --download_path=/var/www/html/$DOWNLOADPATH --downloadcomps --download-metadata -n >> $LOG
+    cd /var/www/html/$DOWNLOADPATH/$REPO
+    createrepo -v /var/www/html/$DOWNLOADPATH/$REPO -g comps.xml >> $LOG
+done
 
 echo \# `date +%Y-%m-%d` - END REPOSYNC \# >> $LOG
-echo \# `date +%Y-%m-%d` - START CREATEREPO \# >> $LOG
-
-cd /var/www/html/local-rhel-7-repo/rhel-7-server-rpms/
-createrepo -v /var/www/html/local-rhel-7-repo/rhel-7-server-rpms/ -g comps.xml >> $LOG
-
-echo \# `date +%Y-%m-%d` - END CREATEREPO \# >> $LOG
-
 exit 0
