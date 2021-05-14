c28b22832f8ab49fef2e9a100c171e918da7f156
Joerg Kastning <jkastning@my-it-brain.de>
2021-05-14 08:50:34 +0200

    First version of script

---
+++ bash/clone-kvm-guest-wrapper.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+# Author: Joerg Kastning <joerg (dot) kastning (at) my-it-brain (dot) de>
+# License: MIT
+# Description: Wrapper Script to clone and sysprep a KVM guest template
+
+# variables
+TEMPLATE_NAME=${1}
+GUEST_NAME=${2}
+
+# functions
+usage(){
+  cat << EOF
+    Wrapper Script to clone and sysprep a KVM guest template
+
+    usage: $0 ARG1 ARG2
+
+    ARG1  Name of the template to clone
+    ARG2  Name of the new kvm guest domain to create 
+EOF
+}
+
+clone(){
+  virt-clone --original "${TEMPLATE_NAME}" --name "${GUEST_NAME}" --auto-clone
+}
+
+sysprep(){
+  virt-sysprep --operations -ssh-userdir --hosname "${GUEST_NAME}" -d "${GUEST_NAME}"
+}
+
+# main
+clone
+sysprep
